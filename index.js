0a2193b5b64eee56867acb40ff798a5673a56f55
Arcelinda <arcysantoa@gmail.com>
2022-04-27 17:12:50 -0100

    modified index.js, add middleware

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require ('express')
+const res = require('express/lib/response')
 
 const app = express()
 
 app.listen ('3000')
 app.route('/').get( (req,res) => res.send('Hello'))
-app.route('/Sobre').get((req, res) => res.send('Hello sobre'))
+//app.route('/Sobre').get((req, res) => res.send('Hello sobre'))
 
 //app.get('/', (req, res) => res.send('Minha API'));
+
+//Middleware
+app.use (express.json())
+
+app.route('/').post ( (req,res) => console.log(req.boby))
