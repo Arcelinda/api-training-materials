78e6add41b980df5c5b8995237425f1f18aac0fc
anilson <anilsonmonteiro@gmail.com>
2022-04-28 12:34:38 -0100

    add delete implementation

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require ('express')
+const express = require('express')
 //const bodyParser = require('body-parser') // importando body-parser
 const app = express() // iniciando o express
 
-app.listen ('3000')
+app.listen('3000')
 
-app.route('/').get( (req,res) => res.send('Hello'))
+app.route('/').get((req, res) => res.send('Hello'))
 //app.route('/Sobre').get((req, res) => res.send('Hello sobre'))
 
 //app.get('/', (req, res) => res.send('Minha API'));
 
 //Middleware
-app.use (express.json())
+app.use(express.json())
 
 // usando o parser para pegar a informação do POST
 //app.use(bodyParser.urlencoded({ extended: true }))
@@ -21,16 +21,38 @@ let author = "Arcelinda"
 // Rotas
 
 //app.route('/').post ( (req,res) => console.log(req.body))
-app.route('/').post ( (req,res) => res.send(req.body))
+app.route('/').post((req, res) => res.send(req.body))
 
-app.route('/').put ((req,res) => {
+app.route('/').put((req, res) => {
     author = req.body.author
     res.send(author)
 
 })
 
-app.route('/').get ((req,res) => res.send(author))
+app.route('/').get((req, res) => res.send(author))
 
-app.route('/:identificador').delete ((req,res) => {
-    res.send(req.params.identificador)
+app.route('/:identificador').delete((req, res) => {
+    let idToDelete = req.params.identificador
+    let result = []
+    for (index = 0; index < data.length; index++) {
+
+        if (data[index].id === idToDelete) { // procurar o id do objecto que pretende eliminar
+            console.log("Removido: ", data[index])
+            result = data.splice(index, 1)           // remove o objecto da "base de dados" em memoria
+        }
+    }
+    console.log(data)                       // verificar se objecto foi removido
+
+    res.send(result)      // retorna a resposta da requisicao
 })
+
+let data = [
+    {
+        author: "Name",
+        id: '1'
+    },
+    {
+        author: "Author2",
+        id: '2'
+    }
+]
