889626704731f2b27b1e87a25ac57aaf3fcfe8fb
Arcelinda <arcysantoa@gmail.com>
2022-04-28 12:56:23 -0100

    trazer variaveis de postman para data  ----data.push(req.body)

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express() // iniciando o express
 app.listen('3000')
 
-app.route('/').get((req, res) => res.send('Hello'))
+//app.route('/').get((req, res) => res.send('Hello'))
+
 //app.route('/Sobre').get((req, res) => res.send('Hello sobre'))
 
 //app.get('/', (req, res) => res.send('Minha API'));
@@ -14,7 +15,13 @@ app.use(express.json())
 let author = "Arcelinda"
 // Rotas
 //app.route('/').post ( (req,res) => console.log(req.body))
-app.route('/').post((req, res) => res.send(req.body))
+
+
+app.route('/').post((req, res) => {
+    data.push(req.body)      
+    res.send(req.body)
+
+})
 
 app.route('/').put((req, res) => {
     author = req.body.author
@@ -40,13 +47,4 @@ app.route('/:identificador').delete((req, res) => {
     res.send(result)      // retorna a resposta da requisicao
 })
 
-let data = [
-    {
-        author: "Name",
-        id: '1'
-    },
-    {
-        author: "Author2",
-        id: '2'
-    }
-]
+let data = []
