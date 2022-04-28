b1a49c4e6a35671d3fe71f3a889442d039e83b0b
Arcelinda <arcysantoa@gmail.com>
2022-04-28 10:45:41 -0100

    index.js add route put

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require ('express')
-const res = require('express/lib/response')
-
-const app = express()
+//const bodyParser = require('body-parser') // importando body-parser
+const app = express() // iniciando o express
 
 app.listen ('3000')
+
 app.route('/').get( (req,res) => res.send('Hello'))
 //app.route('/Sobre').get((req, res) => res.send('Hello sobre'))
 
@@ -12,4 +12,21 @@ app.route('/').get( (req,res) => res.send('Hello'))
 //Middleware
 app.use (express.json())
 
-app.route('/').post ( (req,res) => console.log(req.boby))
+// usando o parser para pegar a informação do POST
+//app.use(bodyParser.urlencoded({ extended: true }))
+//app.use(bodyParser.json())
+
+//variaveis
+let author = "Arcelinda"
+// Rotas
+
+//app.route('/').post ( (req,res) => console.log(req.body))
+app.route('/').post ( (req,res) => res.send(req.body))
+
+app.route('/').put ((req,res) => {
+    author = req.body.author
+    res.send(author)
+
+})
+
+app.route('/').get ((req,res) => res.send(author))
